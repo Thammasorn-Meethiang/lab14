67c7087a0a8a78d35c7ea903e4ba85f47df60747
Pleum <thammasorn_m@cmu.ac.th>
2026-01-13 22:29:54 +0700

    first problem commit

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
-
 template <typename T>
 void insertionSort(T d[],int N){
+	for (int i = 1; i < N; i++) {
+			int key = d[i];
+			int j = i - 1;
+			while (j >= 0 && d[j] < key){
+				d[j + 1] = d[j];
+				j--;
+			}
+			d[j + 1] = key;
+			cout << "Pass " << i <<":";
+			for (int i = 0; i < N; i++){
+					cout << d[i]<<" ";
+				}
+				cout << "\n";
 
-}
+		}
+	}
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
